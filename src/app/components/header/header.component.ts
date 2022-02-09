0befe9474f01faf60fd0529bb74e7751e46bc3ed
mariam-rashwan <mariamrashwan23@gmail.com>
2022-02-09 05:44:51 +0200

    navbar&footer

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+
+  navbarfixed:boolean = false;
+  @HostListener('window:scroll',['$event'])onScroll(){
+    if(window.scrollY >100){
+      this.navbarfixed = true;
+    }
+    else{
+      this.navbarfixed = false;
+    }
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
