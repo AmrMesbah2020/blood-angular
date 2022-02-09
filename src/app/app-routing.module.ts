f031e2f40ef4af9206c4aa9de29b62c57bf29138
mariamRaslan <mariamraslan@gmail.com>
2022-02-09 23:21:30 +0200

    eligibilty form quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { EligibilityQuizComponent } from './components/eligibility-quiz/eligibility-quiz.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'eligibilty-quiz',component:EligibilityQuizComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EligibilityQuizComponent } from './components/eligibility-quiz/eligibility-quiz.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DashCounterComponent } from './components/dash-counter/dash-counter.component';
@@ -10,13 +13,16 @@ import { DashCounterComponent } from './components/dash-counter/dash-counter.com
 @NgModule({
   declarations: [
     AppComponent,
+    EligibilityQuizComponent,
     HeaderComponent,
     FooterComponent,
     DashCounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
