ed5cc8b4e9c0e995074ac906dc0c1cbd0a5505a3
mariam-rashwan <mariamrashwan23@gmail.com>
2022-02-18 15:04:28 +0200

    api admin

--- src/app/components/admin/articles/articles.component.ts
+++ src/app/components/admin/articles/articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/article';
 
 @Component({
   selector: 'app-articles',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
+
+
+  article:Article=new Article();
+  articles:Article[]=[];
+  router: any;
+  private _httpClient: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+
+    if(localStorage.getItem('Token')==null){
+      this.router.navigate(['/login']);
   }
+}
+// public addPost(postData: Object) {
+//   let endPoints = "/posts"
+//   this._httpClient.post('https://jsonplaceholder.typicode.com/posts'), postData)
+//   .subscribe((data: any) => {
+//     console.log(data);
+//   });
+// }
+
 
 }
