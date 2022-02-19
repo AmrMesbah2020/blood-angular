aad109080d1169c6bdc9ed2cf04e272c81053ade
SamarMohamed1 <samar.mohamed.hassan@gmail.com>
2022-02-19 15:09:41 +0200

    done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,11 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { NotificationsService } from 'src/app/services/notifications.service';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { json } from 'stream/consumers';
+import { Request } from 'src/app/models/request';
 
 @Component({
   selector: 'app-header',
@@ -24,9 +31,58 @@ export class HeaderComponent implements OnInit {
     }
   }
   
-  constructor() { }
+  flag:any='';
+  notificationInfo=new Request();
+
+  constructor(private toastr: NotificationsService,private router:Router,private _httpClint:HttpClient) { }
 
   ngOnInit(): void {
+    
   }
+
+//  Notify():void{
+
+//   this._httpClint.get(`http://127.0.0.1:8000/api/getNotification`).subscribe(
+//     (response:any)=>{
+//       console.log(JSON.parse(response.data));
+//       this.flag=response.data.id;
+//       console.log(this.flag);
+      
+//       this.toastr.toastrWarningOnTap('Notification ya sya3','am amr w samar','requests');
+
+//     },
+//     (error:any)=>{
+//       console.log(error.error);
+//       // alert(error.error);
+//     }
+//   )
+//  }
+  setFalg:any=localStorage.setItem('flag','');
+ notification:any=setInterval( ()=>{
+
+  this._httpClint.get(`http://127.0.0.1:8000/api/getNotification`).subscribe(
+    (response:any)=>{
+      // console.log(JSON.parse(response.data));
+      this.notificationInfo=JSON.parse(response.data).data;
+
+     
+      // console.log(this.flag);
+
+      if( ){
+        
+      
+        this.toastr.toastrWarningOnTap('Notification ya sya3','am amr w samar','requests');
+
+      }
+
+    },
+    (error:any)=>{
+      console.log(error.error);
+      // alert(error.error);
+    }
+  )
+ 
+
+ }, 15000);
 
 }
