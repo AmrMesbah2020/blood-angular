78adbf2e5e7f8c1b7ca6ebbbf40617ce596b5c46
AmrMesbah2020 <amr.mohamed.mesbah@gmail.com>
2022-03-06 20:33:02 +0200

    make please

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/blood'));
+app.use(express.static(__dirname+'dist/blood'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/blood'}),
-);
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname,'dist/blood','index.html'))
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
